c2000db19f80abd9cba24db5bfbfefa953b35227
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-03-23 20:33:34 +0100

    Add training with simple network

--- session05/DQN_all.py
+++ session05/DQN_all.py
@@ -1,4 +1,21 @@
 import gymnasium as gym
+import DQNAgent
+import torch.nn as nn
+
+
+class DQN(nn.Module):
+    def __init__(self, input_size, action_size):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 24)
+        self.fc2 = nn.Linear(24, 48)
+        self.fc3 = nn.Linear(48, action_size)
+        self.relu = nn.Tanh()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        return self.fc3(x)  # Linear output (Q-values for each action)
+
 
 # Retrieve all environment IDs
 env_ids = list(gym.envs.registry.keys())
@@ -16,3 +33,11 @@ for env_id in env_ids:
     )
 
     print(f"Threshold for {env_id}: {threshold}")
+
+    DQNAgent.trainModel(
+        env_id,
+        dqnClass=DQN,
+        fileName=env_id.replace("/", "_"),
+        retrain=False,
+        epochs=1000,
+    )
