b3ab9b2961cdfc5a87cf7aabb0f7cafd4283b9d2
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-18 16:49:24 +0200

    Enhance europeCities.py to normalize data and create training/validation batches for temperature prediction

--- session09/europeCities.py
+++ session09/europeCities.py
@@ -1,6 +1,7 @@
 import os
 import kagglehub
 import pandas as pd
+import numpy as np
 
 
 folder = kagglehub.dataset_download("orvile/european-cities-weather-prediction-dataset")
@@ -11,12 +12,49 @@ df = pd.read_csv(fname, delimiter=",", encoding="utf-8")
 # Drop the column DATE and MONTH
 df = df.drop(columns=["DATE", "MONTH"])
 
+# Turn the rest into numberic values
+df = df.apply(pd.to_numeric, errors="coerce")
+
 city_names = [
     col.replace("_temp_mean", "") for col in df.columns if col.endswith("_temp_mean")
 ]
 
 cities = {c: {} for c in city_names}
 
-# Get the raw data
+num_train_samples = int(0.75 * len(df))
+num_val_samples = len(df) - num_train_samples
+
+print("num_train_samples:", num_train_samples)
+print("num_val_samples:", num_val_samples)
+
+
+# Get the data
 for city in city_names:
-    cities[city]["raw_data"] = df.loc[:, df.columns.str.startswith("city")]
+    # Raw data
+    data = df.loc[:, df.columns.str.startswith(city)]
+    cities[city]["raw_data"] = data
+
+    # Normalize the data (this normalizes all using only the training data)
+    mean = data[:num_train_samples].mean(axis=0)
+    data -= mean
+    std = data[:num_train_samples].std(axis=0)
+    data /= std
+
+    # Temperature data
+    cities[city]["temperature"] = df[f"{city}_temp_mean"].values
+
+    # Make batches of 5 days
+    windowsT = np.lib.stride_tricks.sliding_window_view(
+        data[:num_train_samples], (5, data[:num_train_samples].shape[1])
+    ).squeeze(axis=1)
+    windowsV = np.lib.stride_tricks.sliding_window_view(
+        data[num_train_samples:], (5, data[num_train_samples:].shape[1])
+    ).squeeze(axis=1)
+
+    targetsT = cities[city]["temperature"][5 : num_train_samples + 1]
+    targetsV = cities[city]["temperature"][num_train_samples + 4 :]
+
+    cities[city]["trainX"] = windowsT
+    cities[city]["trainY"] = targetsT
+    cities[city]["valX"] = windowsV
+    cities[city]["valY"] = targetsV
