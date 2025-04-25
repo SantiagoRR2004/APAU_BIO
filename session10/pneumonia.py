8cfed83eb821456f6ba45389a9a7ec1e1116d842
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-25 14:53:19 +0200

    Initial files

---
+++ session10/pneumonia.py
@@ -0,0 +1,57 @@
+import medmnist
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+print(medmnist.__version__)
+from medmnist import PneumoniaMNIST
+
+
+# Dataset info
+download = True
+info = medmnist.INFO["pneumoniamnist"]
+print(info)
+DataClass = getattr(__import__("medmnist"), info["python_class"])
+
+# Data transforms
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+    ]
+)
+
+# Load data
+train_dataset = DataClass(split="train", transform=transform, download=download)
+
+healthy_images = []
+pneumonia_images = []
+
+for img, label in train_dataset:
+    if label.item() == 0:
+        healthy_images.append(img.squeeze())  # remove channel dimension
+    if len(healthy_images) == 12:
+        break
+
+for img, label in train_dataset:
+    if label.item() == 1:
+        pneumonia_images.append(img.squeeze())  # remove channel dimension
+    if len(pneumonia_images) == 12:
+        break
+
+# Create mosaic
+fig, axes = plt.subplots(3, 4, figsize=(5, 4))
+fig.suptitle("Healthy (Label = 0) Images from PneumoniaMNIST", fontsize=16)
+for i, ax in enumerate(axes.flat):
+    ax.imshow(healthy_images[i], cmap="gray")
+    ax.axis("off")
+plt.tight_layout()
+
+# Create mosaic
+fig, axes = plt.subplots(3, 4, figsize=(5, 4))
+fig.suptitle("Non-healthy (Label = 0) Images from PneumoniaMNIST", fontsize=16)
+for i, ax in enumerate(axes.flat):
+    ax.imshow(pneumonia_images[i], cmap="gray")
+    ax.axis("off")
+plt.tight_layout()
+
+plt.show()
