73bb4706e03443fab0dcffaac3d1e35684cc67bb
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-27 13:27:58 +0200

    Dropout

--- session08/CowsVsSheepsBasic.py
+++ session08/CowsVsSheepsBasic.py
@@ -13,21 +13,25 @@ class CowsVsSheepsBasic(CowsVsSheeps):
             ),  # self.nChannels × 32 × 3 × 3 + 32
             # (32, self.img_dim - 2, self.img_dim - 2)
             torch.nn.ReLU(),
+            torch.nn.Dropout(0.25),
             torch.nn.MaxPool2d(kernel_size=2),
             # (32, self.img_dim - 4, self.img_dim - 4)
             torch.nn.Conv2d(32, 32, kernel_size=3),  # 32 × 32 × 3 × 3 + 32
             # (32, self.img_dim - 6, self.img_dim - 6)
             torch.nn.ReLU(),
+            torch.nn.Dropout(0.25),
             torch.nn.MaxPool2d(kernel_size=2),
             # (32, self.img_dim - 8, self.img_dim - 8)
             torch.nn.Conv2d(32, 32, kernel_size=3),  # 32 × 32 × 3 × 3 + 32
             # (32, self.img_dim - 10, self.img_dim - 10)
             torch.nn.ReLU(),
+            torch.nn.Dropout(0.25),
             torch.nn.MaxPool2d(kernel_size=2),
             # (32, self.img_dim - 12, self.img_dim - 12)
             torch.nn.Conv2d(32, 32, kernel_size=3),  # 32 × 32 × 3 × 3 + 32
             # (32, self.img_dim - 14, self.img_dim - 14)
             torch.nn.ReLU(),
+            torch.nn.Dropout(0.25),
             torch.nn.Flatten(),
             torch.nn.Linear(10368, self.num_classes),
         ).to(self.device)
