883a54725417dc3b9bb54ad275eb48caf69d3ce8
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-05-12 20:41:22 +0200

    Remove test that was already in another file

--- session11/GAN_fashion.py
+++ session11/GAN_fashion.py
@@ -392,17 +392,3 @@ if __name__ == "__main__":
     classifier = Classifier()
     classifier.train(test_loader)  # Entrenar con datos de test
     classifier.save_model("Classifier.pth")
-
-    num_samples = 5
-    noise = torch.randn(num_samples, z_dim).to(device)
-    fake_images = G(noise).detach().cpu()
-
-    fake_images = fake_images.view(-1, 1, 28, 28)
-    generated_dataset = GeneratedDataset(fake_images)
-    generated_loader = DataLoader(generated_dataset, batch_size=128, shuffle=False)
-
-    # load model Classifier
-    classifier = Classifier()
-    classifier.load_model("Classifier.pth")
-    classifier.class_names = class_names
-    classifier.predict(generated_loader)
